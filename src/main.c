a5c9b3744342f3a66eb27eaa60c5e788f1979e15
Marko Ristic <markoristic@anus-eater.local>
2022-03-13 13:22:03 -0700

    updated main w/ LED waveform display code

--- src/main.c
+++ src/main.c
@@ -22,32 +22,60 @@ int main(void) {
     SPI1_GPIO_Init();
     SPI1_Init();
 	int i = 0;
+    int j = 0;
+    int k = 0;
+    *(a) = 0;
     int mag;
     //Note that the microphone's L/R pin should be connected to Vdd (3.3v)
 	while(1) { //sampling wave mag from mic]
-        if(i%256){
+        if(k%256){
             SPI_LED_Reset(SPI1);
-        }else if(i%4){
+        }else if(k%4){
             SPI_Set_Green(SPI1);
         }
         else {
             SPI_Set_Off(SPI1);
         }
-        i++;
-        /*SPI_Receive_Waveform(SPI2, &receive);
+        k++;
+        //^^ TESTING CODE
+        /*
+        //vvv READING FROM MIC AND DISPLAY WAVEFORM CODE
+        SPI_Receive_Waveform(SPI2, &receive);
         //decode receive to get magnitude value.
         mag = 0;
         for(i = 0; i < 16; i++){
             mag += receive%2;
             receive = receive/2;
         }
-        mag = mag/2;
+        mag = mag/2; 
+        //mag should now be equal to a value E [0,8]
+        //we can have 0 not be displayed on the LED (lowest possible val), and 1-8 correspond to height of max LED
         for(i = 1; i < 32; i++){
             *(a + i) = *(a);
         }
         *(a) = mag;
-        //mag should now be equal to a value E [0,8]
-        //we can have 0 not be displayed on the LED (lowest possible val), and 1-8 correspond to height of max LED
+        //^^^ updating array of LED heights
+        //vvv Sequence of display instructions
+        for(i = 31; i >= 0; i--){
+            if(i%2) { //normal behavior display up to mag,
+                for(j = 0; j < 8; j++){
+                    if(j < *(a+i)){
+                        SPI_Set_Green(SPI1);
+                    } else[
+                        SPI_Set_Off(SPI1);
+                    ]
+                }
+            } else{ //inverse as going from top to bottom instead of bottom to top
+                for(j = 0; j < 8; j++){
+                    if(j < 8 - *(a+i)){
+                        SPI_Set_Off(SPI1);
+                    } else{
+                        SPI_Set_Green(SPI1);
+                    }
+                }
+            }
+        }
+        SPI_LED_Reset(SPI1); //send reset signal so board updates
         */
 	}
 
